dbf2d360a585f19c3ad4f753775e0f79b84187e7
Johanna Gerdts <jg@mavenlist.eu>
2021-10-19 16:30:57 +0200

    done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,19 +1,36 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+  let result;
+  if (num1 < num2){
+    result = num2;
+  }
+  else {
+    result = num1;
+  }
+  return result;
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
-
-function findLongestWord() {}
+function findLongestWord(arr) {
+  let longestWord ='';
+  for (let i=0; i < arr.length; i++){
+    if (arr[i].length > longestWord.length)
+    longestWord = arr[i];
+  }
+  return longestWord;
+}
 
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(arr) {
+  return arr.reduce((a,b) => a+b, 0)
+}
 
 
 
@@ -26,13 +43,23 @@ function sum() {}
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(arr) {
+  let sum = arr.reduce((a,b) => a+b, 0)
+  return sum/arr.length
+}
 
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+function averageWordLength(arr) {
+  let charCount = [];
+  for (let i=0; i<arr.length; i++){
+    charCount.push(arr[i].length)
+  }
+  let sum = charCount.reduce((a,b) => a+b, 0)
+  return sum/charCount.length
+}
 
 // Bonus - Iteration #4.1
 function avg() {}
@@ -52,14 +79,17 @@ const wordsUnique = [
   'bring'
 ];
 
-function uniquifyArray() {}
-
+function uniquifyArray(arr) {
+  return arr.filter((value, index) => arr.indexOf(value) === index);
+}
 
 
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
-function doesWordExist() {}
+function doesWordExist(arr, word) {
+  return arr.includes(word);
+}
 
 
 
@@ -78,8 +108,9 @@ const wordsCount = [
   'matter'
 ];
 
-function howManyTimes() {}
-
+function howManyTimes(arr, word) {
+  return arr.reduce((a,b) => (b ===word ? a + 1 : a), 0);
+}
 
 
 // Iteration #8: Bonus
